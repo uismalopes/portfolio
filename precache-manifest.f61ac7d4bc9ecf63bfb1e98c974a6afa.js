0b6515e3bef21ae788f9419a69c72c6f83e547fe
Uisma Lopes <uismalopes@gmail.com>
2020-06-09 17:56:41 -0300

    Updates

---
+++ precache-manifest.f61ac7d4bc9ecf63bfb1e98c974a6afa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c821727792b165e548c5305f6cc9e4e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5eb8dd513615f0495cca",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ac552871b532a68a58d7",
+    "url": "/portfolio/static/css/main.ad7f7e20.chunk.css"
+  },
+  {
+    "revision": "5eb8dd513615f0495cca",
+    "url": "/portfolio/static/js/2.4d153549.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.4d153549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac552871b532a68a58d7",
+    "url": "/portfolio/static/js/main.ca4eaa46.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "feab97475572c14cccd02c62ae544f3a",
+    "url": "/portfolio/static/media/logo.feab9747.svg"
+  },
+  {
+    "revision": "5cd77f84cf4691ce244a813e77261e55",
+    "url": "/portfolio/static/media/thumb_angularJSListaTarefas.5cd77f84.jpg"
+  },
+  {
+    "revision": "cce7e6e703b98b7447aaf27dfec86b20",
+    "url": "/portfolio/static/media/thumb_flappyBird.cce7e6e7.jpg"
+  },
+  {
+    "revision": "2a63f20ef3b2bb0329bf82d7632da82c",
+    "url": "/portfolio/static/media/thumb_jsListaCompras.2a63f20e.jpg"
+  },
+  {
+    "revision": "f21ac453fce951555306cb85de0389df",
+    "url": "/portfolio/static/media/thumb_leccor.f21ac453.jpg"
+  },
+  {
+    "revision": "17736e77eb73e586f9da1f22ca7637c6",
+    "url": "/portfolio/static/media/thumb_novosNegociosIvoire.17736e77.jpg"
+  },
+  {
+    "revision": "a8e1582088147c9b18b2ca7ae907a95b",
+    "url": "/portfolio/static/media/thumb_verdegente.a8e15820.jpg"
+  }
+]);
